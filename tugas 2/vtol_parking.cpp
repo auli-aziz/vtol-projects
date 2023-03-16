a3020681796df8b8b9b049279a6ab1d7dc93e599
Aulia Aziz <auliaanugrahaziz@gmail.com>
2023-03-17 00:25:59 +0700

    modularize parking meter

--- tugas 2/vtol_parking.cpp
+++ tugas 2/vtol_parking.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <chrono>
 #include <iomanip>
 
 /*
@@ -9,96 +8,108 @@ if more than 24h, dicount/reduce by 10 000 per 24h
 output the duration of time of parking
 */
 
-int main(){
-    int inputMenu, hours1, minutes1, seconds1, hours2, minutes2, seconds2, start, end, diff,
-    diff_hours, diff_minutes, days, diff_days;
-    double total_fee = 0, initial_fee = 3000;
-    char inputDay;
+void parking_meter();
 
+int main(){
+    int inputMenu = 1, loop = 1;
+    
     std::cout << "WELCOME TO THE PARKING METER 1.0" << std::endl;
     std::cout << "=====================================" << std::endl;
     std::cout << "Enter your option:\n1. START\n2. QUIT\nInput: ";
     std::cin >> inputMenu;
     std::cout << "=====================================" << std::endl;
-    
-    if(inputMenu == 1){
 
-        //input time
-        do{
-            std::cout << "Enter starting time (HH:MM): ";
-            std::cin >> std::setw(2) >> hours1;
-            std::cin.ignore();
-            std::cin >> std::setw(2) >> minutes1;
-            seconds1 = hours1 * 3600 + minutes1 * 60;
-            
-            if(hours1 < 0 || minutes1 < 0 || seconds1 >= 86400)
-                std::cout << "Error! Invalid starting time" << "\n";
-        }while(hours1 < 0 || minutes1 < 0 || seconds1 >= 86400);
-
-        std::cout << "Did you exit on a different day? (Y/n) ";
-        
-        do{
-            std::cin >> inputDay;
-            if(!(inputDay == 'y' || inputDay == 'Y' || inputDay == 'n' || inputDay == 'N')) 
-                std::cout << "Invalid input! ";
-        }while(!(inputDay == 'y' || inputDay == 'Y' || inputDay == 'n' || inputDay == 'N'));
-
-        if(inputDay == 'y' || inputDay == 'Y'){
-            std::cout << "Enter the amount of days parked: ";
-            std::cin >> days;
-        }
-        
-        do{
-            std::cout << "Enter exit time (HH:MM): ";
-            std::cin >> std::setw(2) >> hours2;
-            std::cin.ignore();
-            std::cin >> std::setw(2) >> minutes2;
-            seconds2 = hours2 * 3600 + minutes2 * 60;
-            
-            if(hours2 < 0 || minutes2 < 0 || seconds2 >= 86400)
-                std::cout << "Error! Invalid exit time" << "\n";
-        }while(hours2 < 0 || minutes2 < 0 || seconds2 >= 86400);
-
+    while(inputMenu == 1){
+        parking_meter(); 
         std::cout << "=====================================" << std::endl;
+        std::cout << "Enter your option:\n1. START\n2. QUIT\nInput: ";
+        std::cin >> inputMenu;
+        std::cout << "=====================================" << std::endl;
+    } 
+    std::cout << "THANK YOU FOR USING PARKING METER 1.0 \n";    
+    
+    return 0;
+}
 
-        //calculation (can be modularized)
-        start = seconds1;
+void parking_meter(){
+    int hours1 = 0, minutes1 = 0, seconds1 = 0, hours2 = 0, minutes2 = 0, seconds2 = 0, start = 0, end = 0, diff = 0, diff_hours = 0, diff_minutes = 0, days = 0, diff_days = 0, total_fee = 0;
+    char inputDay;
+    
+    //input time
+    do{
+        std::cout << "Enter starting time (HH:MM): ";
+        std::cin >> std::setw(2) >> hours1;
+        std::cin.ignore();
+        std::cin >> std::setw(2) >> minutes1;
+        seconds1 = (hours1 * 3600) + (minutes1 * 60);
+        
+        if(hours1 < 0 || minutes1 < 0 || seconds1 >= 86400)
+            std::cout << "Error! Invalid starting time" << "\n";
+    }while(hours1 < 0 || minutes1 < 0 || seconds1 >= 86400);
 
-        if(days >= 1){
-            end = days * 86400 + seconds2;
-        }else{
-            end = seconds2;
-        }
+    std::cout << "Did you exit on a different day? (Y/n) ";
+    
+    do{
+        std::cin >> inputDay;
+        if(!(inputDay == 'y' || inputDay == 'Y' || inputDay == 'n' || inputDay == 'N')) 
+            std::cout << "Invalid input! ";
+    }while(!(inputDay == 'y' || inputDay == 'Y' || inputDay == 'n' || inputDay == 'N'));
 
-        diff = abs(end - start);
-        diff_days = diff / 86400;
-        diff_hours = (diff % 86400) / 3600;
-        diff_minutes = (diff % 3600) / 60;
+    if(inputDay == 'y' || inputDay == 'Y'){
+        std::cout << "Enter the amount of days parked: ";
+        std::cin >> days;
+    }
+    
+    do{
+        std::cout << "Enter exit time (HH:MM): ";
+        std::cin >> std::setw(2) >> hours2;
+        std::cin.ignore();
+        std::cin >> std::setw(2) >> minutes2;
+        seconds2 = (hours2 * 3600) + (minutes2 * 60);
+        
+        if(hours2 < 0 || minutes2 < 0 || seconds2 >= 86400)
+            std::cout << "Error! Invalid exit time" << "\n";
+    }while(hours2 < 0 || minutes2 < 0 || seconds2 >= 86400);
 
+    std::cout << "=====================================" << std::endl;
 
-        //ternary oparator idea for the s*
-        std::cout << "Total parking time: " << diff_days << " day(s) " << diff_hours << 
-        " hour(s) and " << diff_minutes << " minute(s) \n";
+    //calculation (can be modularized)
+    start = seconds1;
 
-        //fee calculation & output
-        if(diff_hours != 0 || diff_days != 0){
-            total_fee += initial_fee;
-            if(diff_hours >= 2 || diff_days != 0){
-                if(days == 0) total_fee += 1000 * (diff_hours - 1);
-                else total_fee += days * 23000;
-                //if parking time is x hours and y minutes it adds additional fee
-                if(diff_minutes >= 1) total_fee += 1000;
-            }
-            
-            //discount fee
-            if(diff_days >= 1){
-                total_fee -= days * 10000;
-            }
-            std::cout << "Total payment: Rp" << total_fee << "\n";
-        } else{
-            std::cout << "Your parking is still Free!";
-        }
+    if(days >= 1){
+        end = (days * 86400) + seconds2;
     }else{
-        std::cout << "THANK YOU FOR USING PARKING METER 1.0 \n";
+        end = seconds2;
+    }
+
+    diff = abs(end - start);
+    diff_days = diff / 86400;
+    diff_hours = (diff % 86400) / 3600;
+    diff_minutes = (diff % 3600) / 60;
+
+    //ternary oparator idea for the s*
+    std::cout << "Total parking time: " << diff_days << " day(s) " << diff_hours << 
+    " hour(s) and " << diff_minutes << " minute(s) \n";
+
+    //fee calculation & output
+    if(diff_hours != 0 || diff_days != 0){
+        total_fee += 3000;
+        if(diff_hours >= 2 || diff_days != 0){
+            if(days == 0) 
+                total_fee += 1000 * (diff_hours - 1);
+            else 
+                total_fee += (days * 23000);
+            //if parking time is x hours and >0 minutes it adds 1000
+            if(diff_minutes >= 1) 
+                total_fee += 1000;
+        }
+        
+        //discount fee
+        if(diff_days >= 1){
+            total_fee -= (days * 10000);
+        }
+        std::cout << "Total payment: Rp" << total_fee << "\n";
+    } else{
+        std::cout << "Your parking is still Free!";
     }
 }
